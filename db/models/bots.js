1a361991d1d66875100fefcdf035fce01092d2ec
David Dias <daviddias.p@gmail.com>
2013-12-27 14:54:01 +0000

    update bot schema

--- db/models/bots.js
+++ db/models/bots.js
@@ -1,27 +1,25 @@
 var mongoose = require('mongoose');
 
 var botSchema = new mongoose.Schema({
-  bot_id: {type: String, unique: true},
-  bot_name: String,
-  bot_key: String,
-  bot_state: {type: Boolean, default: false},
-  bot_visible: Boolean, // After having the user
+  id: {type: String, unique: true},
+  name: String,
+  key: String,
+  state: {type: Boolean, default: false},
+  visible: Boolean, // After having the user
   address: String,
   commands: [String], // List of commands available to execute
-  livefeedurl: String,
-  photourl: String,
+  liveFeedURL: String,
+  photoURL: String,
   description: String,
 });
 
 botSchema.statics.findByBotId = function (botId, cb) {
-  this.find({ bot_id: botId }, cb);
+  this.find({ id: botId }, cb);
 };
 
 botSchema.statics.findAll = function (cb) {
   this.find({},cb);
 };
-
-
 
 
 var Bot = module.exports = mongoose.model('Bot', botSchema);
